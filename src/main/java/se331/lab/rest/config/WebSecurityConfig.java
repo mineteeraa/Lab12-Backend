f3906b7d4331a063b7a6b9a9e20461561ba6539b
mineteeraa <patteera_mine@hotmail.com>
2021-10-03 23:14:05 +0700

    [6] Define the role of user and admin

--- src/main/java/se331/lab/rest/config/WebSecurityConfig.java
+++ src/main/java/se331/lab/rest/config/WebSecurityConfig.java
@@ -67,6 +67,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/events").permitAll()
                 .antMatchers(HttpMethod.GET, "/organizers").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/events").hasRole("ADMIN")
 
                 .anyRequest().authenticated();
 
