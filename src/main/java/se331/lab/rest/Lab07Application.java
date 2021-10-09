95054fe04dfe43c6dc98ee4396201026dadff718
mineteeraa <patteera_mine@hotmail.com>
2021-10-09 15:21:37 +0700

    FIX

--- src/main/java/se331/lab/rest/Lab07Application.java
+++ src/main/java/se331/lab/rest/Lab07Application.java
@@ -19,7 +19,7 @@ public class Lab07Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://54.221.54.201:8001")
                         .exposedHeaders("x-total-count");
             }
         };
